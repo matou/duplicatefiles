8c04dd5e26354e8d29ed7be20cb7c7cb66ac4a22
matou <m_matou@gmx.net>
2011-01-19 23:12:05 +0100

    only hashing if several files of that size exist; debugged a debug bug

--- duplicatefiles.py
+++ duplicatefiles.py
@@ -1,8 +1,7 @@
 import os,sys,hashlib,logging
 
 # init logging
-logging.basicConfig(level=logging.INFO)
-
+level = logging.INFO
 # very verbose output?
 SPAM = False
 
@@ -15,19 +14,26 @@ loglevel = {"debug":logging.DEBUG,"info":logging.INFO,"warning":logging.WARNING,
 # parse arguments:
 for i in range(len(sys.argv)):
     if sys.argv[i] == "-l":
-        logging.basicConfig(level=loglevel[sys.argv[i+1].lower()])
+        level = loglevel[sys.argv[i+1].lower()]
         if sys.argv[i+1].lower() == "spam":
             SPAM = True
     if sys.argv[i] == "-t":
         threshold = int(sys.argv[i+1])
 
+logging.basicConfig(level=level)
+
 def spam(msg):
     if SPAM:
-        logging.debug(msg)
+        logging.debug("SPAM:%s" % msg)
 
-def process_file(path):
-    "returns a tuple (hashsum, size)"
-    spam("processing %s" % path)
+logging.debug("this is debugged")
+spam("spam")
+logging.debug("threshold is %d" % threshold)
+
+
+def hash_file(path):
+    "returns hashsum as string"
+    spam("hashing %s" % path)
     f = open(path)
     md5 = hashlib.md5()
     while True:
@@ -38,7 +44,7 @@ def process_file(path):
             break
         md5.update(byte)
     f.close()
-    return (md5.hexdigest(), os.path.getsize(path))
+    return md5.hexdigest()
     
 # first collect all files that aren't directories or symlinks
 logging.info("searching for files in current directory ('%s')" 
@@ -57,14 +63,14 @@ while len(dirs) > 0:
             if os.path.getsize(f) <= threshold:
                 spam("ignored %s" % f)
                 continue
-            key = process_file(f)
+            key = os.path.getsize(f)
             if not files.has_key(key):
                 files[key] = []
             files[key].append(f)
             filecounter += 1
             # debug
-            if filecounter%100 == 0:
-                logging.debug("processed %d files" % filecounter)
+            if filecounter%10000 == 0:
+                logging.debug("found %d files" % filecounter)
             # end debug
         elif os.path.isdir(f):
             dirs.append(f)
@@ -72,10 +78,24 @@ while len(dirs) > 0:
 
 logging.info("found %d files bigger than %d bytes" % (filecounter, threshold))
 
+same = {}
+# hash files that are of same size
+filecounter = 0
+for size in files:
+    if len(files[size]) > 1:
+        for f in files[size]:
+            hashsum = hash_file(f)
+            if not same.has_key((size, hashsum)):
+                same[(size,hashsum)] = []
+            same[(size,hashsum)].append(f)
+            filecounter += 1
+            if filecounter%100 == 0:
+                logging.debug("hashed %d files" % filecounter)
+
 # output the result
-for key in files:
-    if len(files[key]) > 1:
+for key in same:
+    if len(same[key]) > 1:
         print "these files are the same: ",
-        for f in files[key]:
+        for f in same[key]:
             print "%s, " % f,
         print ""
